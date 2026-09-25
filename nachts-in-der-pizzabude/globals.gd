1578e2c3ede6bbce70ba5e6c99ddd29d9f6bbb24
ravengalqueen <holfrank61@gmail.com>
2026-09-25 20:54:09 +0200

    basic energy backend

--- nachts-in-der-pizzabude/globals.gd
+++ nachts-in-der-pizzabude/globals.gd
@@ -1,5 +1,5 @@
 extends Node
-
+var energy = 100.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +9,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func discharge(amount):
+	amount = clamp(amount, 0.1, 100.0)
+	energy = clamp((energy-amount), 0.0, 100.0)
+
+func recharge(amount):
+	amount = clamp(amount, 0.1, 100.0)
+	energy = clamp((energy+amount), 0.0, 100.0)
