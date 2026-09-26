c3fafc5afecbf9512b38fb0c74b328a607f751b8
Vadereimer <Vader-Zleimer@gmx.de>
2026-09-26 14:35:07 +0200

    uhm stuff friedrich Merzbear

--- nachts-in-der-pizzabude/Nightselect.gd
+++ nachts-in-der-pizzabude/Nightselect.gd
@@ -13,35 +13,35 @@ func _process(delta: float) -> void:
 
 
 func _on_night_1_button_down() -> void:
-	globals.night = 1
+	globals.night == 1
 	get_tree().change_scene_to_file("res://Office1.tscn")
 
 
 func _on_night_2_button_down() -> void:
-	if globals.nightunl >= 2:
-		globals.night = 2
+	if globals.nightunl <= 2:
+		globals.night == 2
 		get_tree().change_scene_to_file("res://Office1.tscn")
 
 
 func _on_night_3_button_down() -> void:
-	if globals.nightunl >= 3:
-		globals.night = 3
+	if globals.nightunl <= 3:
+		globals.night == 3
 		get_tree().change_scene_to_file("res://Office1.tscn")
 
 
 func _on_night_4_button_down() -> void:
-	if globals.nightunl >= 4:
-		globals.night = 4
+	if globals.nightunl <= 4:
+		globals.night == 4
 		get_tree().change_scene_to_file("res://Office1.tscn")
 
 
 func _on_night_5_button_down() -> void:
-	if globals.nightunl >= 5:
-		globals.night = 5
+	if globals.nightunl <= 5:
+		globals.night == 5
 		get_tree().change_scene_to_file("res://Office1.tscn")
 
 
 func _on_night_6_button_down() -> void:
 	if globals.nightunl == 6:
-		globals.night = 6
+		globals.night == 6
 		get_tree().change_scene_to_file("res://Office1.tscn")
--- nachts-in-der-pizzabude/YOUWON.gd
+++ nachts-in-der-pizzabude/YOUWON.gd
@@ -11,38 +11,38 @@ func _process(delta: float) -> void:
 	pass
 	
 
-func _on_Mainmenu_button_pressed():
-	globals.hours == 0
-	get_tree().change_scene_to_file("res://Mainmenu1.tscn")
-	if globals.nightunl == 1:
-		globals.night = 2
-	elif globals.nightunl == 2:
-		globals.night = 3
-	elif globals.nightunl == 3:
-		globals.night = 4
-	elif globals.nightunl == 4:
-		globals.night = 5
-	elif globals.nightunl == 5:
-		globals.night = 6
-
 
 func _on_nextnight_button_down() -> void:
-	globals.hours == 0
+	globals.hours = -6
 	globals.night = +1
 	get_tree().change_scene_to_file("res://Office1.tscn")
 	if globals.nightunl == 1:
-		globals.night = 2
+		globals.nightunl = +1
+		print("worked")
 	elif globals.nightunl == 2:
-		globals.night = 3
+		globals.nightunl = +1
 	elif globals.nightunl == 3:
-		globals.night = 4
+		globals.nightunl = +1
 	elif globals.nightunl == 4:
-		globals.night = 5
+		globals.nightunl = +1
 	elif globals.nightunl == 5:
-		globals.night = 6
+		globals.nightunl = +1
 		
 
 
 
-func _on_main_menu_button_down() -> void:
-	pass # Replace with function body.
+func _on_mainmenu_button_down() -> void:
+	globals.hours = -6
+	get_tree().change_scene_to_file("res://Mainmenu1.tscn")
+	if globals.nightunl == 1:
+		globals.nightunl = +1
+		print("worked")
+	elif globals.nightunl == 2:
+		globals.nightunl = +1
+	elif globals.nightunl == 3:
+		globals.nightunl = +1
+	elif globals.nightunl == 4:
+		globals.nightunl = +1
+	elif globals.nightunl == 5:
+		globals.nightunl = +1
+	
